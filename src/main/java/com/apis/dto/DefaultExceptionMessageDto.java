e1359d1b1dfa05a2d418058f57187675370d4ec1
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-02-15 14:37:13 -0500

    Ticketing project with Rest

---
+++ src/main/java/com/apis/dto/DefaultExceptionMessageDto.java
@@ -0,0 +1,13 @@
+package com.apis.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class DefaultExceptionMessageDto {
+
+    private String message;
+}
